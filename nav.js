84c3e70b9941df21f938efa5dbb362f27e49e788
jeuecreative <j.eue.creative@gmail.com>
2026-09-20 20:39:03 +0200

    20.09.-Demo

--- nav.js
+++ nav.js
@@ -17,3 +17,24 @@
     });
   });
 })();
+
+/* Demo-Einbettung (Sektion #demo): hält die Skalierung des iframes an der
+   tatsächlichen Rahmenbreite. Ohne JavaScript greift der CSS-Vorgabewert .78 –
+   die Vorschau sitzt dann nur bei ungefähr 1000 px Rahmenbreite bündig,
+   bricht aber nichts. Auf Seiten ohne Demo passiert hier gar nichts. */
+(function () {
+  'use strict';
+  var win = document.querySelector('.df__window');
+  if (!win) return;
+
+  var fit = function () {
+    win.style.setProperty('--df-scale', win.clientWidth / 1280);
+  };
+
+  if ('ResizeObserver' in window) {
+    new ResizeObserver(fit).observe(win);
+  } else {
+    window.addEventListener('resize', fit, { passive: true });
+  }
+  fit();
+})();
